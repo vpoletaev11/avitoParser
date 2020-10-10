918e13fa15ba7971d718a719996549301c511fe5
vpoletaev11 <vpoletaev11@gmail.com>
2020-10-10 04:27:33 +0300

    Add GetPrice

---
+++ scrapper/scrapper.go
@@ -0,0 +1,53 @@
+package scrapper
+
+import (
+	"crypto/tls"
+	"io/ioutil"
+	"net/http"
+	"strconv"
+	"strings"
+)
+
+func GetPrice(link string) (int, error) {
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{
+			MaxVersion: tls.VersionTLS12,
+		},
+	}
+	client := &http.Client{Transport: tr}
+
+	req, err := http.NewRequest("GET", link, nil)
+	if err != nil {
+		return 0, err
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return 0, err
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	bodyString := string(body)
+
+	priceStr, _ := getStringBetweenTwoStrings(bodyString, `"dynx_price":`, ",")
+	price, err := strconv.Atoi(priceStr)
+	if err != nil {
+		return 0, err
+	}
+
+	return price, nil
+}
+
+func getStringBetweenTwoStrings(str string, startS string, endS string) (result string, found bool) {
+	s := strings.Index(str, startS)
+	if s == -1 {
+		return result, false
+	}
+	newS := str[s+len(startS):]
+	e := strings.Index(newS, endS)
+	if e == -1 {
+		return result, false
+	}
+	result = newS[:e]
+	return result, true
+}
